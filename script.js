73f514d615f54005c99a8c88e92c420e745d82a9
eminentmmark <markswenso@gmail.com>
2023-10-22 21:56:38 +0300

    added scroll reveal

--- script.js
+++ script.js
@@ -33,19 +33,6 @@
 	};
 
 // scroll reveal
-	// ScrollReveal({
-	// 	// reset: true,
-	// 	distance: '80px',
-	// 	duration: 2000,
-	// 	delay: 200
-	// });
-
-	// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-	// ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-	// ScrollReveal().reveal('.home-content h1, .about-img, .contact-col', { origin: 'left' });
-	// ScrollReveal().reveal('.home-content p, .about-content, .brands', { origin: 'right' });
-
-// scroll reveal
 	ScrollReveal({
 		// reset: true,
 		distance: '80px',
@@ -53,10 +40,10 @@
 		delay: 200
 	});
 
-	ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-	ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-	ScrollReveal().reveal('.home-content h1, .about-img, .contact-col, .services3-container', { origin: 'left' });
-	ScrollReveal().reveal('.home-content p, .about-content, .brands', { origin: 'right' });
+	ScrollReveal().reveal('.home-content, .header', { origin: 'top' });
+	ScrollReveal().reveal('.home-img, .services-container, .contact form', { origin: 'bottom' });
+	ScrollReveal().reveal('.home-content h1, .about-img, .contact-col', { origin: 'left' });
+	ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 	const typed = new Typed('.multiple-text', {
