98612cb8b596fc5ee3bf1c9e9a8b15e44ccd0e39
eminentmmark <markswenso@gmail.com>
2023-10-22 15:41:54 +0300

    added scroll reveal

--- script.js
+++ script.js
@@ -45,6 +45,19 @@
 	// ScrollReveal().reveal('.home-content h1, .about-img, .contact-col', { origin: 'left' });
 	// ScrollReveal().reveal('.home-content p, .about-content, .brands', { origin: 'right' });
 
+// scroll reveal
+	ScrollReveal({
+		// reset: true,
+		distance: '80px',
+		duration: 2000,
+		delay: 200
+	});
+
+	ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+	ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+	ScrollReveal().reveal('.home-content h1, .about-img, .contact-col, .services3-container', { origin: 'left' });
+	ScrollReveal().reveal('.home-content p, .about-content, .brands', { origin: 'right' });
+
 // typed js
 	const typed = new Typed('.multiple-text', {
 		strings: ['amettite', 'beads', 'brass', 'leather', 'marbles', 'onyx', 'paracode', 'silver'],
